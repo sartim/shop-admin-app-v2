f042aebb0dd0ad23860895d733fc85fa7885ca2f
Tim <>
2024-09-15 22:04:26 +0300

    Add service

---
+++ Services/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ShopAdminApp.Services
+{
+    public class AuthService : BaseService
+    {
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var json = $"{{\"email\": \"{email}\", \"password\": \"{password}\"}}";
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            HttpResponseMessage response = await _client.PostAsync("api/v1/auth/generate-jwt", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                try
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    await SecureStorage.SetAsync("jwt", jsonResponse);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.WriteLine("Possible that device doesn't support secure storage on device.");
+                }
+            }
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Services/BaseService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ShopAdminApp.Services
+{
+    public class BaseService
+    {
+        protected readonly HttpClient _client;
+
+        public BaseService()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://flask-shop-api.vercel.app/");
+        }
+
+        protected async Task<T> SendRequestAsync<T>(HttpMethod method, string url, object data = null, string authToken = null)
+        {
+
+            HttpRequestMessage request = new HttpRequestMessage(method, url);
+
+            if (data != null)
+            {
+                string jsonData = JsonConvert.SerializeObject(data);
+                request.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+
+            if (authToken != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+            }
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(jsonResponse);
+            }
+            else
+            {
+                Console.WriteLine($"Request failed: {response.StatusCode}");
+                return default;
+            }
+        }
+    }
+}
