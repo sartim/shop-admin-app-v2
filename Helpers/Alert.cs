e0ca994fe764dd736e24556886908f0e330f087b
Tim <write2sartim@gmail.com>
2024-10-07 20:19:28 +0300

    Add alert helper

---
+++ Helpers/Alert.cs
@@ -0,0 +1,19 @@
+﻿
+
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace shupavuApp.Helpers
+{
+    public class Alert
+    {
+        public static async Task ToastAlert(string text)
+        {
+            ToastDuration duration = ToastDuration.Short;
+            double fontSize = 14;
+            var toast = Toast.Make(text, duration, fontSize);
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            await toast.Show(cts.Token);
+        }
+    }
+}
